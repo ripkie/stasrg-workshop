a1c204a82255d966a68b950dc932bb39ec4fd49c
ripkie <rifki.dupon07@gmail.com>
2026-05-02 05:32:22 +0700

    fix: /server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+const express = require("express");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
